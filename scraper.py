69c6b4bfd4cd1ae152914516bd6aff66021a05db
GianlucaGL <35380912+GianlucaGL@users.noreply.github.com>
2018-01-27 12:04:34 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -13,7 +13,7 @@ record = {}
 root = lxml.html.fromstring(html)
 sections = root.cssselect("tr")
 for section in sections:
-  print "This section is: {}".format(section.text_content())
+  print "This section is: {}".format(section.text_content().encode('ascii', 'ignore'))
   # names = root.cssselect("td div a")
   # if section = root.cssselect("td div a")
   # print section.text.encode('ascii', 'ignore')
